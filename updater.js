d77699e25958a6b42d43ce93bd256c5a936052d5
Benny <benny123tw@gmail.com>
2021-08-11 18:16:25 +0800

    adding format bytes fn

--- updater.js
+++ updater.js
@@ -6,8 +6,20 @@ ipcRenderer.on("message", function (event, data) {
   } else {
         var progressBar = document.getElementById("innerBar");
         var innerText = document.getElementById("innerText");
-        message.innerText = 'Download Speed:' + data.bytesPerSecond;
+        message.innerText = 'Download Speed:' + formatBytes(data.bytesPerSecond);
         progressBar.style.width = data.percent + '%';
-        innerText.innerText = `${data.percent.toFixed(1)}% ${data.transferred.toFixed(2)} / ${data.total.toFixed(2)}`; 
+        innerText.innerText = `${data.percent.toFixed(1)}% ${formatBytes(data.transferred)} / ${formatBytes(data.total)}`; 
   }
 });
+
+function formatBytes(bytes, decimals = 2) {
+  if (bytes === 0) return '0 Bytes';
+
+  const k = 1024;
+  const dm = decimals < 0 ? 0 : decimals;
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+}
